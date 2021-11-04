b0d011be30e2f2c5835f595d476db0e9f147bee1
long.chen <long.chen@ctsmed.com>
2021-11-04 17:10:34 +0800

    ..

--- code/DataGenerator/run_generation.sh
+++ code/DataGenerator/run_generation.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 -m debugpy --listen 192.168.111.165:5678 --wait-for-client ./run_generation.py
+python3 -m debugpy --listen 192.168.111.165:5678 --wait-for-client ./run_generation.py --UnrealProjectName AbandonedFactory --languages Latin
